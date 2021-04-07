963867cc2afd029c93c95576172dffe288d5561d
marochedelnoy <marochedelnoy@icloud.com>
2021-04-07 09:11:47 +0200

    project

---
+++ SML/SML/AppShell.xaml.cs
@@ -0,0 +1,19 @@
+﻿using SML.ViewModels;
+using SML.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace SML
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+    }
+}
---
+++ SML/SML/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using SML.Models;
+using SML.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SML.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
